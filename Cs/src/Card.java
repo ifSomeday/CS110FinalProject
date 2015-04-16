1d195560b1d89e637fe9b0b695da9ac53d40d267
Will <willrice21@gmail.com>
2015-04-16 18:22:34 -0400

    Implemented basic card class, removed dsfdsf.java

---
+++ Cs/src/Card.java
@@ -0,0 +1,25 @@
+
+public class Card {
+	
+	final int SPADES = 4, HEARTS = 3, CLUBS = 2, DIAMONDS = 1;
+	
+	private int suit, rank;
+	
+	public Card(int suit, int rank){
+		this.suit = suit;
+		this.rank = rank;
+	}
+	
+	public int getSuit(){
+		return(this.suit);
+	}
+	
+	public int getRank(){
+		return(this.rank);
+	}
+	
+	public boolean equals(Card card){
+		return(this.rank == card.getRank());
+	}
+
+}
